c5e0ca15002b080dabe234096fde8d52c109f9c0
kalil reis de sisto <kalilreisdesisto@gmail.com>
2019-10-29 17:49:52 -0300

    server added

---
+++ client/build/precache-manifest.58b40b3b1ff68cc7e312cb69f549e126.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04477be5a94947b309ea21d50d9c4713",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45eac32c71bae934290e",
+    "url": "/static/css/2.8bed955a.chunk.css"
+  },
+  {
+    "revision": "45eac32c71bae934290e",
+    "url": "/static/js/2.771d44e2.chunk.js"
+  },
+  {
+    "revision": "4fc770b860a852f53274",
+    "url": "/static/js/main.de6aa22c.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
